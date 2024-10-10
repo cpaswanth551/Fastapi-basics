9d5bb3749dae7cb5b4db7a35ba2d0875316155f6
cpaswanthpalayad <cpaswanth551@gmail.com>
2024-10-10 17:51:47 +0530

    class dependency and global dependency  multilevel dependency

--- app/custom_log.py
+++ app/custom_log.py
@@ -1,3 +1,7 @@
-def log(tag="", message=""):
-    with open("log.txt", "w+") as log:
-        log.write(f"{tag}:{message}")
+from fastapi import Request
+
+
+def log(tag="MyTag", message="no messages", request: Request = None):
+    with open("log.txt", "a+") as log:
+        log.write(f"{tag}:{message}\n")
+        log.write(f"\t{request.url}\n")
--- app/router/dependencies.py
+++ app/router/dependencies.py
@@ -1,21 +1,53 @@
 from fastapi import APIRouter, Depends, Request
 
+from app.custom_log import log
 
-router = APIRouter(prefix="/dependencies", tags=["dependencies"])
+
+router = APIRouter(
+    prefix="/dependencies", tags=["dependencies"], dependencies=[Depends(log)]
+)
 
 
-def convert_header(request: Request, seperater: str = "---"):
+# multi level dependency
+
+
+def convert_params(request: Request, separator: str):
+    query = []
+    for key, value in request.query_params.items():
+        query.append(f"{key} {separator} {value}")
+    return query
+
+
+def convert_headers(
+    request: Request, separator: str = "--", query=Depends(convert_params)
+):
     out_headers = []
     for key, value in request.headers.items():
-        out_headers.append(f"{key} {seperater} {value}")
-    return out_headers
+        out_headers.append(f"{key} {separator} {value}")
+    return {"headers": out_headers, "query": query}
 
 
 @router.get("")
-def get_items(header=Depends(convert_header)):
-    return {"item": [1, 2, 3, 4], "headers": header}
+def get_items(test: str, separator: str = "--", headers=Depends(convert_headers)):
+    return {"items": ["a", "b", "c"], "headers": headers}
 
 
 @router.post("/new")
-def create_items(header=Depends(convert_header)):
+def create_items(header=Depends(convert_headers)):
     return {"item": [1, 2, 3, 4], "headers": header}
+
+
+# class based dependency
+
+
+class Account:
+    def __init__(self, name: str, email: str):
+        self.name = name
+        self.email = email
+
+
+@router.post("/user")
+def create_user(
+    name: str, email: str, password: str, account: Account = Depends(Account)
+):
+    return {"name": account.name, "email": account.email}
